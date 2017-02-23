3df40e74ed2ff088806462443afa0e77ef358348
jkhoang313 <jkhoang313@gmail.com>
2017-02-23 16:06:35 -0500

    Done.

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,26 @@
 require_relative 'stack'
 
 class Queue
+  def initialize
+    @adding = Stack.new
+    @removing = Stack.new
+  end
 
+  def enqueue(data)
+    @adding.push(data)
+  end
+
+  def dequeue
+    @adding.size.times do
+      @removing.push(@adding.pop)
+    end
+
+    next_pop = @removing.pop
+
+    @removing.size.times do
+      @adding.push(@removing.pop)
+    end
+
+    next_pop
+  end
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Stack
+  attr_accessor :stack
 
+  def initialize
+    @stack = []
+  end
+
+  def push(data)
+    @stack << data
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def size
+    @stack.size
+  end
 end
